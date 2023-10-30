0a765e9e0f94b1b07742dbe2b8249aeb7cab96e3
Alexandr Levichev <alex-levichev@mail.ru>
2023-10-30 11:27:08 +0300

    added automationPracticeForm

--- src/test/java/SimpleJunitTest.java
+++ src/test/java/SimpleJunitTest.java
@@ -23,8 +23,8 @@ public class SimpleJunitTest {
 
     void automationPracticeFormTest() {
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Bill");
-        $("#lastName").setValue("Gates");
+        $("#firstName").setValue("Aleksandr");
+        $("#lastName").setValue("Levichev");
         $("#userEmail").setValue("Bill@bill.com");
         $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("8999881105");
@@ -40,12 +40,28 @@ public class SimpleJunitTest {
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#currentAddress").setValue("Moscow oblast");
         $("#uploadPicture").uploadFile(new File("C:\\Users\\alex-\\IdeaProjects\\demoqa-tests\\src\\test\\picture"));
+        $("#state").scrollTo();
         $("#state").click();
         $("#react-select-3-input").val("NCR").pressEnter();
         $("#city").click();
         $("#react-select-4-input").val("Delhi").pressEnter();
         $("#submit").pressEnter();
+        // Checks
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Aleksandr Levichev"));
+        $(".table-responsive").shouldHave(text("Bill@bill.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("8999881105"));
+        $(".table-responsive").shouldHave(text("01 December,1900"));
+        $(".table-responsive").shouldHave(text("Maths, Computer Science"));
+        $(".table-responsive").shouldHave(text("Reading, Music, Sports"));
+        $(".table-responsive").shouldHave(text("picture"));
+        $(".table-responsive").shouldHave(text("Moscow oblast"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
 
+
+
+        //Check
         //submit
 
 
@@ -58,10 +74,7 @@ public class SimpleJunitTest {
 
 
 
-        //     $("#hobbies-checkbox-1").click();
-     //   $("#hobbies-checkbox-2").click();
-     //   $("").setValue("");
-     //   $("").setValue("");
+
 
      //   $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
